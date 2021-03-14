6242e6584724e4fa180b9bf6f14f1e9aba8a7364
DESKTOP-5VMHJT8\Mohamed Amin Khattab <khattabmohamed174@gmail.com>
2021-03-15 00:22:47 +0200

    Added win detection to player

--- Assets/___LevelEditingProject/Scripts/PlayerController.cs
+++ Assets/___LevelEditingProject/Scripts/PlayerController.cs
@@ -45,4 +45,12 @@ public class PlayerController : MonoBehaviour
 
 
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Winzone"))
+        {
+            //todo cinemachine flyover
+            Debug.Log("won");
+        }
+    }
 }
